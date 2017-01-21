76242db8f3b64c417027e0694a1015aacde57e25
Jonathan Burd <mail@jonathanburd.com>
2017-01-21 14:58:25 -0600

    Merge branch 'master' of https://github.com/arconium/batsNeonNoir
    # Conflicts:
    #	batsNeonNoir/Assets/Art/_Working Folders/Jaremie/Neonized_SOLID.jpg.meta

---
+++ batsNeonNoir/Assets/Scripts/WaveIndicator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveIndicator : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 pos = player.transform.position;
+        Vector3 thing = mouse - pos;
+        gameObject.transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(thing.y, thing.x) * 180 / 3.141f, Vector3.forward);
+	}
+}
