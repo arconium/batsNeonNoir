17c8f9a1cdd515b309670aa7c5155ba4ac8f84c6
Scott <silentbob666@gmail.com>
2017-01-21 20:17:18 -0600

    Added snake enemies

---
+++ batsNeonNoir/Assets/Scripts/SnakeMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SnakeMovement : MonoBehaviour {
+
+
+	public Vector3[] points;
+	private int current = 0;
+	public float speed = 1.0f;
+
+
+	void Start () {
+
+	}
+
+	void Update () {
+		if ((transform.position - points[current]).magnitude < 0.05f) {
+			current = (current + 1) % points.Length;
+		}
+		transform.position = Vector3.MoveTowards(transform.position, points[current], speed * Time.deltaTime);
+	}
+}
