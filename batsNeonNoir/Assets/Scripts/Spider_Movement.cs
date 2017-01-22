3350aa34ec5b8c00ff1e651c84bc05b5d6b2af50
Shane <ongxx103@umn.edu>
2017-01-21 22:48:26 -0600

    Your friendly neighbourhood spider murderers are in

---
+++ batsNeonNoir/Assets/Scripts/Spider_Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Spider_Movement : MonoBehaviour {
+
+	public Vector3[] points;
+	public bool toWeb = false;
+	private int current = 0;
+	public float speed = 1.0f;
+	public Transform goal;
+
+	void Start () {
+
+	}
+
+	void Update () {
+		Vector3 thing;
+		if (toWeb) {
+			thing = goal.position;
+		} else {
+			if ((transform.position - points [current]).magnitude < 0.05f) {
+				current = (current + 1) % points.Length;
+			}
+			thing = points [current];
+		}
+		thing.z = transform.position.z;
+		transform.position = Vector3.MoveTowards(transform.position, thing, speed * Time.deltaTime);
+
+	}
+}
---
+++ batsNeonNoir/Assets/Scripts/WebCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCollider : MonoBehaviour {
+
+	public GameObject spider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void OnTriggerEnter2D(Collider2D collision) {
+		spider.GetComponent<Spider_Movement>().goal = collision.transform;
+		spider.GetComponent<Spider_Movement> ().toWeb = true;
+	}
+
+	public void OnTriggerExit2D(Collider2D collision) {
+		spider.GetComponent<Spider_Movement> ().toWeb = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Component thing = spider.GetComponent<Spider_Movement>()
+	}
+}
