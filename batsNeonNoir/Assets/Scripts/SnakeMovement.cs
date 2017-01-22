f8ad6e749f8ed293a626a1f3d4bcd693020942d0
Jonathan Burd <mail@jonathanburd.com>
2017-01-22 16:16:18 -0600

    4PM build. Snake animation fixed.

--- batsNeonNoir/Assets/Scripts/SnakeMovement.cs
+++ batsNeonNoir/Assets/Scripts/SnakeMovement.cs
@@ -9,7 +9,8 @@ public class SnakeMovement : MonoBehaviour {
 	public Vector3[] points;
 	private int current = 0;
 	public float speed = 1.0f;
-
+    public bool horizontal;
+    public bool pos;
 
 	void Start () {
 
@@ -18,7 +19,21 @@ public class SnakeMovement : MonoBehaviour {
 	void Update () {
 		if ((transform.position - points[current]).magnitude < 0.05f) {
 			current = (current + 1) % points.Length;
-		}
+            if (horizontal && (pos && points[current].x < transform.position.x || !pos && points[current].x > transform.position.x))
+            {
+                pos = !pos;
+                Vector3 scale = transform.localScale;
+                scale.x *= -1;
+                transform.localScale = scale;
+            }
+            if (!horizontal && (pos && points[current].y < transform.position.y || !pos && points[current].y > transform.position.y))
+            {
+                pos = !pos;
+                Vector3 scale = transform.localScale;
+                scale.x *= -1;
+                transform.localScale = scale;
+            }
+        }
 		transform.position = Vector3.MoveTowards(transform.position, points[current], speed * Time.deltaTime);
 		
 	}
