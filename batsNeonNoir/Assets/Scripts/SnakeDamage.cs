a5c5d8837825fed059016f01cdd15e38b3702753
Scott <silentbob666@gmail.com>
2017-01-21 21:41:21 -0600

    Script for snake

---
+++ batsNeonNoir/Assets/Scripts/SnakeDamage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeDamage : MonoBehaviour {
+
+    // Keep this here
+    public int enemyDamage = 1;
+    //public GameObject player;
+
+
+    void Start () {
+		
+	}
+
+
+    public void OnTriggerEnter2D( Collider2D other2)
+    {
+		// Attack animation
+
+        // Give damage
+        BatController giveDamage = other2.gameObject.GetComponent<BatController>();
+        //BatController giveDamage = player.GetComponent<BatController>();
+        giveDamage.playerHealth -= enemyDamage;
+        
+    }
+
+
+    void FixedUpdate () {
+	
+        
+        	
+	}
+}
